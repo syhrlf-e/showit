30dd875f114ffe8c3112e6ff51a5c7bb981239c8
Rul/E <136341720+syhrlf-e@users.noreply.github.com>
2026-02-16 22:02:37 +0700

    refactor: Update ESLint configuration to flat config with `eslint-config-next` and migrate project to ES modules.

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,35 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export default defineConfig([
-  globalIgnores(["dist"]),
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
+      ...tseslint.configs.recommended,
+      ...compat.extends("next/core-web-vitals"),
     ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
   },
-]);
+);
